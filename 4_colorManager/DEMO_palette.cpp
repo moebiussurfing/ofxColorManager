78effd33faff75cd82670661473a86cf7887adf3
moebiussurfing <moebiussurfing@users.noreply.github.com>
2019-08-06 22:20:37 +0200

    Update DEMO_palette.cpp

--- 4_colorManager/DEMO_palette.cpp
+++ 4_colorManager/DEMO_palette.cpp
@@ -22,92 +22,6 @@ void DEMO_palette::setup() {
 
 
 //--------------------------------------------------------------
-void DEMO_palette::draw() {
-    // DEMO 1
-
-    if (ENABLE_DEMO1) {
-        ofPushMatrix();
-        ofPushStyle();
-        ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
-
-        for (int i = 0; i < this->locations.size(); i++) {
-            ofSetColor(this->colors[i]);
-
-//        float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 10;
-            float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 20;
-//        float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 40;
-
-            ofDrawCircle(this->locations[i], radius);
-        }
-        ofPopStyle();
-        ofPopMatrix();
-    }
-
-    //--
-
-    // DEMO 2 - ROTATING RECTANGLES
-
-    if (ENABLE_DEMO2) {
-        this->cam.begin();
-
-        ofPushMatrix();
-        ofPushStyle();
-
-        float scaleRects = 2.f;
-
-//          float radius = 25;
-        float radius = 300;
-//          float radius = 150;
-
-//          int iDeg = 25;
-        int iDeg = 36;
-
-//          int iDeg = 360/ (2*palette.size());
-          int iCol = 0;
-
-        for (int deg = 0; deg < 360; deg += iDeg) {
-            float x = radius * cos(deg * DEG_TO_RAD);
-            float y = radius * sin(deg * DEG_TO_RAD);
-            ofColor c;
-
-            //-
-
-//            // 1. color from gradient
-//            float cnt = ofMap(deg, 0, 360, 0.f, 1.f);
-//            c = getColorAtPercent(cnt);//gradient not in the class...
-
-            // 2. color from palette
-//            c.set((palette[iCol]);
-//            c.set((palette[iCol]->getColor()));
-            iCol++;
-//            iCol = iCol%palette.size();
-            iCol = iCol%palette->size();
-
-            //-
-
-            ofSetColor(c);
-
-//                ofRotateZ(ofGetFrameNum() * 0.25);
-            ofRotateZ(ofGetFrameNum() * 0.01);
-
-            ofPushMatrix();
-            ofTranslate(ofVec3f(x, y, 0));
-            ofRotateX(90);
-            ofRotateY(deg + 90);
-
-            ofRect(ofVec3f(0, 0, 0), scaleRects * 1000, scaleRects * 1000);
-
-            ofPopMatrix();
-        }
-
-        ofPopStyle();
-        ofPopMatrix();
-
-        this->cam.end();
-    }
-}
-
-//--------------------------------------------------------------
 void DEMO_palette::update() {
 
 
@@ -150,8 +64,10 @@ void DEMO_palette::update() {
 
 // 2. palette color: get one of the palette colors
 //        color.set(palette[iColor]);//TODO
+        color.set((*palette)[iColor]);
+
         //TODO
-        color.set(ofRandom(255), ofRandom(255), ofRandom(255));
+//        color.set(ofRandom(255), ofRandom(255), ofRandom(255));
 
         iColor++;
 //        int palSize = palette.size();
@@ -178,5 +94,96 @@ void DEMO_palette::update() {
             this->velocities.erase(this->velocities.begin() + i);
             this->colors.erase(this->colors.begin() + i);
         }
+    }
+}
+
+
+
+//--------------------------------------------------------------
+void DEMO_palette::draw() {
+
+    // DEMO 1 - CIRCLES
+
+    if (ENABLE_DEMO1) {
+        ofPushMatrix();
+        ofPushStyle();
+        ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
+
+        for (int i = 0; i < this->locations.size(); i++) {
+            ofSetColor(this->colors[i]);
+
+//        float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 10;
+            float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 20;
+//        float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 40;
+
+            ofDrawCircle(this->locations[i], radius);
+        }
+        ofPopStyle();
+        ofPopMatrix();
+    }
+
+    //--
+
+    // DEMO 2 - ROTATING RECTANGLES
+
+    if (ENABLE_DEMO2) {
+        this->cam.begin();
+
+        ofPushMatrix();
+        ofPushStyle();
+
+        float scaleRects = 2.f;
+
+//          float radius = 25;
+        float radius = 300;
+//          float radius = 150;
+
+//          int iDeg = 25;
+        int iDeg = 36;
+
+//          int iDeg = 360/ (2*palette.size());
+        int iCol = 0;
+
+        for (int deg = 0; deg < 360; deg += iDeg) {
+            float x = radius * cos(deg * DEG_TO_RAD);
+            float y = radius * sin(deg * DEG_TO_RAD);
+            ofColor c;
+
+            //-
+
+//            // 1. color from gradient
+//            float cnt = ofMap(deg, 0, 360, 0.f, 1.f);
+//            c = getColorAtPercent(cnt);//gradient not in the class...
+
+            // 2. color from palette//TODO
+//            c.set((palette[iCol]);
+            c.set((*palette)[iCol]);
+//            c.set((palette[iCol]->getColor()));
+
+            iCol++;
+//            iCol = iCol%palette.size();
+            iCol = iCol%palette->size();
+
+            //-
+
+            ofSetColor(c);
+
+//                ofRotateZ(ofGetFrameNum() * 0.25);
+            ofRotateZ(ofGetFrameNum() * 0.01);
+
+            ofPushMatrix();
+            ofTranslate(ofVec3f(x, y, 0));
+            ofRotateX(90);
+            ofRotateY(deg + 90);
+
+            ofRect(ofVec3f(0, 0, 0), scaleRects * 1000, scaleRects * 1000);
+
+            ofPopMatrix();
+        }
+
+        ofPopStyle();
+        ofPopMatrix();
+
+        this->cam.end();
     }
 }
