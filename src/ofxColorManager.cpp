4180e29ccb3945c68fad394abfb7bdf808564884
moebiussurfing <moebiussurfing@users.noreply.github.com>
2019-08-08 01:28:34 +0200

    added layout autoLoad.

--- src/ofxColorManager.cpp
+++ src/ofxColorManager.cpp
@@ -1,7 +1,6 @@
 #include "ofxColorManager.h"
 
 
-
 //--------------------------------------------------------------
 ofxColorManager::ofxColorManager()
 {
@@ -281,8 +280,13 @@ void ofxColorManager::setup()
 
     load_group_XML(XML_params, XML_path);
 
+    //-
+
+    // STARTUP
+
     // TODO
-    palette_load("myPalette");
+//    palette_load("myPalette");
+    preset_load(PRESET_name);
 
     //-
 
@@ -2647,46 +2651,13 @@ void ofxColorManager::keyPressed( ofKeyEventArgs& eventArgs )
     // LOAD
 
     else if (key == 'l') {
-//        preset_load(PRESET_name);
-
-        myPresetPalette.setName(PRESET_name);
-        myPresetPalette.setCurveName(PRESET_curveName);
-//        myPresetPalette.setBackgroundColor(color_backGround);//error ofParameter
-        myPresetPalette.setPalette(palette);
-
-        myPresetPalette.preset_load(PRESET_name);
-        vector<ofColor> palette_TEMP = myPresetPalette.getPalette();
-
-        palette_clear();
-        for (int i=0; i<palette_TEMP.size(); i++)
-        {
-            ofColor c;
-            c = palette_TEMP[i];
-            palette_addColor(c);
-        }
-
-        color_backGround = ofColor( myPresetPalette.getBackground() );
-
-//        PRESET_curveName = curveName_BACK;
-////        string *name_BACK;
-////        vector<ofColor> *palette_BACK;
-////        string *curveName_BACK;
-
-//        ofxLoadCamera(cam, "DEMOCam");
+        preset_load(PRESET_name);
     }
 
     // SAVE
 
     else if (key == 's') {
-        myPresetPalette.setName(PRESET_name);
-        myPresetPalette.setCurveName(PRESET_curveName);
-        ofColor c = color_backGround.get();
-        //error if ofParameter
-        myPresetPalette.setBackgroundColor(c);
-        myPresetPalette.setPalette(palette);
-        myPresetPalette.preset_save(PRESET_name);
-
-//        ofxSaveCamera(cam, "DEMOCam");
+        preset_save(PRESET_name);
     }
 
 //--
@@ -3268,13 +3239,6 @@ void ofxColorManager::exit()
     ofRemoveListener(params_palette.parameterChangedE(), this, &ofxColorManager::Changed_CONTROL);
     ofRemoveListener(params_curve.parameterChangedE(), this, &ofxColorManager::Changed_CONTROL);
 
-//    ofxSaveCamera(cam, "DEMOCam");
-
-//    bool ofxSaveCamera(const ofNode &node, string savePath);
-//    Where you pass the camera or ofNode object that you want to save, and the directory where it is going to be saved
-//
-//    Loading is quite much the same.
-
     //--
 
     // ofxGuiPanelsLayout
@@ -3342,44 +3306,72 @@ void ofxColorManager::exit()
 void ofxColorManager::preset_load(string p)
 {
     ofLogNotice("ofxColorManager:preset_load") << p;
-    string path = preset_path+p+".json";
+//    string path = preset_path+p+".json";
 
-    ofFile file(path);
-    if (file.exists())
+    myPresetPalette.setName(p);
+//    myPresetPalette.setName(PRESET_name);
+    myPresetPalette.setCurveName(PRESET_curveName);
+//        myPresetPalette.setBackgroundColor(color_backGround);//error ofParameter
+    myPresetPalette.setPalette(palette);
+
+    myPresetPalette.preset_load(PRESET_name);
+    vector<ofColor> palette_TEMP = myPresetPalette.getPalette();
+
+    palette_clear();
+    for (int i=0; i<palette_TEMP.size(); i++)
     {
-        jsonin ji(file);
-        ji >> presetData;
-
-        ofLogNotice("ofxColorManager:preset_load") << "presetData.name      : " << presetData.name;
-        ofLogNotice("ofxColorManager:preset_load") << "presetData.curveName : " << presetData.curveName;
-        ofLogNotice("ofxColorManager:preset_load") << "presetData.background: " << presetData.background;
-
-        curvesTool.load(preset_path + presetData.curveName + ".yml");
-
-        if (color_backGround_SETAUTO)
-        {
-            color_backGround = presetData.background;
-        }
-
-        ofLogNotice("ofxColorManager:preset_load") << "presetData.palette.size(): " << presetData.palette.size();
-
-        palette_clear();
-
-        for (int i=0; i<presetData.palette.size(); i++)
-        {
-            ofColor c;
-            c = presetData.palette[i];
-            ofLogNotice("ofxColorManager:preset_load") << "addColor:" << ofToString(c) <<" ["<<i<<"]";
-            palette_addColor(c);
-        }
-        ofLogNotice("ofxColorManager:preset_load") << "DONE! preset_load  : " << p;
-        ofLogNotice("ofxColorManager:preset_load") << "palette.size()     :" << palette.size()<<endl;
-        ofLogNotice("ofxColorManager:preset_load") << "btns_palette.size():" << btns_palette.size()<<endl;
+        ofColor c;
+        c = palette_TEMP[i];
+        palette_addColor(c);
     }
-    else
-    {
-        ofLogNotice("ofxColorManager:preset_load") << "FILE '" << path << "' NOT FOUND";
-    }
+
+    color_backGround = ofColor( myPresetPalette.getBackground() );
+
+//        PRESET_curveName = curveName_BACK;
+////        string *name_BACK;
+////        vector<ofColor> *palette_BACK;
+////        string *curveName_BACK;
+
+//        ofxLoadCamera(cam, "DEMOCam");
+
+
+
+//    ofFile file(path);
+//    if (file.exists())
+//    {
+//        jsonin ji(file);
+//        ji >> presetData;
+//
+//        ofLogNotice("ofxColorManager:preset_load") << "presetData.name      : " << presetData.name;
+//        ofLogNotice("ofxColorManager:preset_load") << "presetData.curveName : " << presetData.curveName;
+//        ofLogNotice("ofxColorManager:preset_load") << "presetData.background: " << presetData.background;
+//
+//        curvesTool.load(preset_path + presetData.curveName + ".yml");
+//
+//        if (color_backGround_SETAUTO)
+//        {
+//            color_backGround = presetData.background;
+//        }
+//
+//        ofLogNotice("ofxColorManager:preset_load") << "presetData.palette.size(): " << presetData.palette.size();
+//
+//        palette_clear();
+//
+//        for (int i=0; i<presetData.palette.size(); i++)
+//        {
+//            ofColor c;
+//            c = presetData.palette[i];
+//            ofLogNotice("ofxColorManager:preset_load") << "addColor:" << ofToString(c) <<" ["<<i<<"]";
+//            palette_addColor(c);
+//        }
+//        ofLogNotice("ofxColorManager:preset_load") << "DONE! preset_load  : " << p;
+//        ofLogNotice("ofxColorManager:preset_load") << "palette.size()     :" << palette.size()<<endl;
+//        ofLogNotice("ofxColorManager:preset_load") << "btns_palette.size():" << btns_palette.size()<<endl;
+//    }
+//    else
+//    {
+//        ofLogNotice("ofxColorManager:preset_load") << "FILE '" << path << "' NOT FOUND";
+//    }
 }
 
 
@@ -3387,28 +3379,42 @@ void ofxColorManager::preset_load(string p)
 void ofxColorManager::preset_save(string p)
 {
     ofLogNotice("ofxColorManager:preset_save") << "preset_save: " << p;
-    string path = preset_path+p+".json";
+//    string path = preset_path+p+".json";
 
-    presetData.name = PRESET_name;//TODO:
-    presetData.curveName = "curve01";//TODO:
-    presetData.background = color_backGround.get();
 
-    ofLogNotice("ofxColorManager:preset_save") << "presetData.name      : " << presetData.name;
-    ofLogNotice("ofxColorManager:preset_save") << "presetData.curveName : " << presetData.curveName;
-    ofLogNotice("ofxColorManager:preset_save") << "presetData.background: " << presetData.background;
+//    myPresetPalette.setName(PRESET_name);
+    myPresetPalette.setName(p);
+    myPresetPalette.setCurveName(PRESET_curveName);
+    ofColor c = color_backGround.get();
+    //error if ofParameter
+    myPresetPalette.setBackgroundColor(c);
+    myPresetPalette.setPalette(palette);
+    myPresetPalette.preset_save(PRESET_name);
 
-    curvesTool.save(preset_path+presetData.curveName+".yml");
+//        ofxSaveCamera(cam, "DEMOCam");
 
-    presetData.palette.resize(palette.size());
-    for (int i = 0; i< palette.size(); i++)
-    {
-        presetData.palette[i] = palette[i];
-        ofLogNotice("ofxColorManager:preset_save") << "presetData.palette[" << i << "]: " << ofToString(presetData.palette[i]);
-    }
 
-    ofFile file(path, ofFile::WriteOnly);
-    jsonout jo(file);
-    jo << presetData;
 
-    ofLogNotice("ofxColorManager:preset_save") << "DONE! preset_save: " << p;
+//    presetData.name = PRESET_name;//TODO:
+//    presetData.curveName = "curve01";//TODO:
+//    presetData.background = color_backGround.get();
+//
+//    ofLogNotice("ofxColorManager:preset_save") << "presetData.name      : " << presetData.name;
+//    ofLogNotice("ofxColorManager:preset_save") << "presetData.curveName : " << presetData.curveName;
+//    ofLogNotice("ofxColorManager:preset_save") << "presetData.background: " << presetData.background;
+//
+//    curvesTool.save(preset_path+presetData.curveName+".yml");
+//
+//    presetData.palette.resize(palette.size());
+//    for (int i = 0; i< palette.size(); i++)
+//    {
+//        presetData.palette[i] = palette[i];
+//        ofLogNotice("ofxColorManager:preset_save") << "presetData.palette[" << i << "]: " << ofToString(presetData.palette[i]);
+//    }
+//
+//    ofFile file(path, ofFile::WriteOnly);
+//    jsonout jo(file);
+//    jo << presetData;
+//
+//    ofLogNotice("ofxColorManager:preset_save") << "DONE! preset_save: " << p;
 }
