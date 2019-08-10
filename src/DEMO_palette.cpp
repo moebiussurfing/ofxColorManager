74558c925cf1067f83e3df5b71f92179251e650e
moebiussurfing <moebiussurfing@users.noreply.github.com>
2019-08-10 23:35:05 +0200

    avoid not linked pointer crashes..

--- src/DEMO_palette.cpp
+++ src/DEMO_palette.cpp
@@ -56,7 +56,7 @@ void DEMO_palette::update() {
 //        glm::vec2 mousePos = glm::vec2(ofGetMouseX(), ofGetMouseY()) - glm::vec2(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
 //        startPos = mousePos;
 
-        startPos = glm::vec2(0,0);
+        startPos = glm::vec2(0, 0);
 
         this->locations.push_back(startPos);
 
@@ -73,7 +73,13 @@ void DEMO_palette::update() {
 
 // 2. palette color: get one of the palette colors
 //        color.set(palette[iColor]);//TODO
-        color.set((*palette)[iColor]);
+
+//avoid crash when palette pointer not linked
+        if (palette != nullptr) {
+            color.set((*palette)[iColor]);
+        } else {
+            color.set(ofColor(ofRandom(255)));
+        }
 
         //TODO
 //        color.set(ofRandom(255), ofRandom(255), ofRandom(255));
@@ -83,7 +89,12 @@ void DEMO_palette::update() {
         int palSize = palette->size();
         iColor = iColor % palSize;
 
-        this->colors.push_back(color);
+        if (palette != nullptr) {
+            this->colors.push_back(color);
+
+        } else {
+            this->colors.push_back(ofColor(ofRandom(255)));
+        }
 
 // sort each circle/color speed
 //            this->velocities.push_back(glm::normalize( glm::vec2(palSize/(float)iColor * ofRandom(-1, 1), ofRandom(-1, 1)) ));
@@ -94,7 +105,6 @@ void DEMO_palette::update() {
         this->locations[i] += this->velocities[i];
 
 // distance to erase circles outside screen
-//        int maxCirclesDist = 720;
         int maxCirclesDist = 1440;
 
         if (glm::length(this->locations[i]) > maxCirclesDist) {
@@ -105,7 +115,6 @@ void DEMO_palette::update() {
         }
     }
 }
-
 
 
 //--------------------------------------------------------------
@@ -120,11 +129,7 @@ void DEMO_palette::draw() {
 
         for (int i = 0; i < this->locations.size(); i++) {
             ofSetColor(this->colors[i]);
-
-//        float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 10;
             float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 20;
-//        float radius = (2 * PI * glm::length(this->locations[i])) / 360 * 40;
-
             ofDrawCircle(this->locations[i], radius);
         }
         ofPopStyle();
@@ -142,12 +147,7 @@ void DEMO_palette::draw() {
         ofPushStyle();
 
         float scaleRects = 2.f;
-
-//          float radius = 25;
         float radius = 300;
-//          float radius = 150;
-
-//          int iDeg = 25;
         int iDeg = 36;
 
 //          int iDeg = 360/ (2*palette.size());
@@ -166,12 +166,18 @@ void DEMO_palette::draw() {
 
             // 2. color from palette//TODO
 //            c.set((palette[iCol]);
-            c.set((*palette)[iCol]);
-//            c.set((palette[iCol]->getColor()));
 
-            iCol++;
-//            iCol = iCol%palette.size();
-            iCol = iCol%palette->size();
+            if (palette != nullptr) {
+                c.set((*palette)[iCol]);
+                iCol++;
+                iCol = iCol % palette->size();
+            }
+
+            else
+            {
+                c.set(ofColor(ofRandom(255)));
+                iCol++;
+            }
 
             //-
 
