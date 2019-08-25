bf32df249cef3c1b7429ecfb31b843c70daa87d7
moebiussurfing <moebiussurfing@users.noreply.github.com>
2019-08-26 01:27:03 +0200

    added color quantizer with drag and drop files

---
+++ 6_colorManager/src/ofApp.cpp
@@ -0,0 +1,87 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofEnableAlphaBlending();
+    ofSetFrameRate(30);
+    ofSetLogLevel(OF_LOG_NOTICE);
+
+    ColorManager.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    ColorManager.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    ColorManager.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+    ColorManager.exit();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+    if (key == 'h')
+    {
+        hide = !hide;
+        ColorManager.setVisible(!hide);
+    }
+
+    if (key == OF_KEY_TAB)
+    {
+        palette = ColorManager.getPalette();
+        ofLogNotice("ofApp") << "getPalette";
+
+        for (int i=0; i<palette.size(); i++)
+        {
+            ofLogNotice("ofApp") << ofToString(palette[i]);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    ColorManager.windowResized(w, h);
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
