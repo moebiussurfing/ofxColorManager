c99cbc60c9829fff73470f746e9eadc26adc3adc
moebius surfing <moebiussurfing@gmail.com>
2021-06-22 01:10:35 -0300

    added json palettes importer

--- examples/3-Tool_JSON_Converter/src/ofApp.cpp
+++ examples/3-Tool_JSON_Converter/src/ofApp.cpp
@@ -10,10 +10,10 @@ void ofApp::setup() {
 	//--
 
 	// 1.
+	// Taken from https://github.com/Jam3/nice-color-palettes
+	// A JSON of the top color palettes on ColourLovers.com, as RGB hex strings.
 	if (0)
 	{
-		// Taken from https://github.com/Jam3/nice-color-palettes
-		// A JSON of the top color palettes on ColourLovers.com, as RGB hex strings.
 
 		// set the source file to convert
 		ofFile file("1000.json");
@@ -97,14 +97,13 @@ void ofApp::setup() {
 	//--
 
 	// 2.
-	if (1)
+	// Taken from
+	// https://github.com/sravanrekandar/google-material-color-palette-json
+	if (0)
 	{
-		// Taken from
-		// https://github.com/sravanrekandar/google-material-color-palette-json
-
 		ofFile file("palette.json");
 
-		string kitName = "GOOGLE MATERIAL";
+		string kitName = "GOOGLE MATERIAL 2";
 		string path = "OUTPUT/" + kitName; // set kit folder name
 		string filenameRoot = "Palette"; // set palette names root
 
@@ -182,7 +181,6 @@ void ofApp::setup() {
 						ofColor myColor = kit.palettes[p].palette[c];
 
 						ofJson jc; // color
-
 						jc["r"] = myColor.r;
 						jc["g"] = myColor.g;
 						jc["b"] = myColor.b;
@@ -206,6 +204,206 @@ void ofApp::setup() {
 			}
 		}
 	}
+
+	//--
+
+	// 3.
+	// This part is to generate palettes. But also can be used to
+	// get colors to make a new library like the pantone lib included in Paletto.
+	// This is a more manual converter, not using json methods but "tricky" manual string manipulations.
+	// Taken from
+	// https://github.com/buschtoens/material-colors-json
+	/*
+	{
+		"red-50": "#ffebee",
+		"red-100": "#ffcdd2",
+		"red-200": "#ef9a9a",
+		"red-300": "#e57373",
+		"red-400": "#ef5350",
+		"red-500": "#f44336",
+		"red-600": "#e53935",
+		"red-700": "#d32f2f",
+		"red-800": "#c62828",
+		"red-900": "#b71c1c",
+		"red-a100": "#ff8a80",
+		"red-a200": "#ff5252",
+		"red-a400": "#ff1744",
+		"red-a700": "#d50000",
+
+		"pink-50": "#fce4ec",
+		"pink-100": "#f8bbd0",
+		"pink-200": "#f48fb1",
+		"pink-300": "#f06292",
+		"pink-400": "#ec407a",
+		"pink-500": "#e91e63",
+		"pink-600": "#d81b60",
+		"pink-700": "#c2185b",
+		..
+	*/
+
+	if (1)
+	{
+		ofFile file("colors.json");
+
+		string kitName = "GOOGLE MATERIAL";
+		string path = "OUTPUT/" + kitName; // set kit folder name
+		string filenameRoot = "Palette"; // set palette names root
+
+		string slog;
+
+		static bool bDebug = true;
+
+		ofBuffer buffer(file);
+
+		vector<string> names;
+		bool bNew = false;
+		int amtPalettes = 0;
+		string namePal;
+
+		// clear
+		paletteType p;
+		kit.palettes.clear();
+
+		//int numLine = 0;
+
+		bNew = true;
+		slog += "-> New \n"; // -> new palette starts here
+
+		// Read file line by line
+		for (ofBuffer::Line it = buffer.getLines().begin(), end = buffer.getLines().end(); it != end; ++it)
+		{
+			//numLine++;
+
+			string line = *it;
+			vector<string> words = ofSplitString(line, ":");
+
+			if (line == "  ")
+			{
+				bNew = true;
+				slog += "-> New \n"; // -> new palette starts here
+			}
+
+			if (line == "{" || line == "}" || line == "" || line == " " || words.size() != 2) continue; // skip format or line
+
+			//if (/*line == "{" ||*/ numLine == 1)
+			//{
+			//	bNew = true;
+			//	slog += "--\n"; // -> new palette starts here
+			//	continue;
+			//}
+
+			//--
+
+			string name, hex;
+
+			// hex
+			hex = words[1];
+			bool bvalid = (ofStringTimesInString(hex, "#") == 1);
+			if (!bvalid) continue; // skip line if no # fountd
+			ofStringReplace(hex, "\"", "");
+			ofStringReplace(hex, "#", "");
+			ofStringReplace(hex, ",", "");
+			ofStringReplace(hex, " ", "");
+
+			// name
+			name = words[0];
+			ofStringReplace(name, " ", "");
+			ofStringReplace(name, "\"", "");
+			if (name == "white" || name == "black") continue; // skip these colors..
+
+			//--
+
+			// a palette starts here
+			if (bNew)
+			{
+				bNew = false;
+
+				// queue last loop iteration
+				if (amtPalettes != 0) kit.palettes.push_back(p);
+
+				//-
+
+				p.palette.clear();
+
+				// queue palette name
+				amtPalettes++;
+				int amt = ofStringTimesInString(name, "-");
+				auto snames = ofSplitString(name, "-");
+				if (amt == 1) {
+					namePal = (snames[0]);
+					names.push_back(namePal);
+				}
+				else if (amt == 2) {
+					namePal = (snames[0] + "-" + snames[1]);
+					names.push_back(namePal);
+				}
+
+			}
+
+			// log
+			string stab = "     \t";
+			string str = "name:" + name + stab + " hex:" + hex + "\n";
+			slog += str;
+
+			//-
+
+			// queue color to current palette
+			ofColor myColor;
+			hexToColor(myColor, hex);
+			p.palette.push_back(myColor);
+
+			//-
+
+			////TODO:
+			//// queue
+			//if (amtPalettes == 1) kit.palettes.push_back(p);
+		}
+
+
+		//--
+
+		// kit is filled with all the palettes
+
+		// 2.2 save each palette to his json
+
+		for (int p = 0; p < kit.palettes.size() && p < names.size(); p++)
+		{
+			ofJson jp; // palette
+
+			for (int c = 0; c < kit.palettes[p].palette.size(); c++)
+			{
+				ofColor myColor = kit.palettes[p].palette[c];
+
+				ofJson jc; // color
+				jc["r"] = myColor.r;
+				jc["g"] = myColor.g;
+				jc["b"] = myColor.b;
+				jc["a"] = myColor.a;
+				jp.push_back(jc);
+			}
+
+			//--
+
+			int i = p + 1;
+			string suffix = "";
+			//suffix += ofToString(i < 1000 ? "0" : "");
+			//suffix += ofToString(i < 100 ? "0" : "");
+			suffix += ofToString(i < 10 ? "0" : "");
+			suffix += ofToString(i);
+
+			string filename = /*filenameRoot + "_" +*/ suffix + "_" + ofToUpper(names[p]) + ".json";
+
+			// save json
+			ofSavePrettyJson(path + "/" + filename, jp);
+		}
+
+		//--
+
+		slog += "\n names:" + ofToString(names) + "\n";
+		text = slog;
+
+		cout << slog << endl;
+	}
 }
 
 //--------------------------------------------------------------
@@ -222,17 +420,4 @@ void ofApp::draw() {
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
-
-	//for (int p = 0; p < kit.palettes.size(); p++) {
-	//	cout << "------------------------" << endl;
-	//	cout << "palette #" << p << endl;
-	//	for (int c = 0; c < kit.palettes[p].palette.size(); c++) {
-	//		cout << "color #" << c << " " << kit.palettes[p].palette[c] << endl;
-	//	}
-	//}
-
-	//    ofJson j;
-	//    vector<ofColor> p = kit.palettes[p].palette;
-	//    to_json(j, p);
-	//    ofSavePrettyJson ("myPalette", j);
 }
--- examples/3-Tool_JSON_Converter/src/ofApp.h
+++ examples/3-Tool_JSON_Converter/src/ofApp.h
@@ -5,60 +5,68 @@
 
 /*
 
-	Example tool to convert json files templates that have palettes configurations.
-	You should use this as a template and to convert your code to import other templated json files with palettes of colors.
+	NOTE
+
+	Example tool to convert JSON files templates that have palettes configurations.
+	You should use this as a template and to convert your code to import other templated JSON files with palettes of colors.
+	You will generate palette presets files compatible with Paletto.
+	Also you can generate compatible colors libs like the bundled Pantone library.
 
 */
 
 
-//#include "ofxSerializer.h"
+#include "ofxSerializer.h"
 
 /*
  These string to hex conversions aren't trivial.
  */
+ //--------------------------------------------------------------
 static int stringToHex(string hex) {
-    int aHex;
-    stringstream convert(hex);
-    convert >> std::hex >> aHex;
-    return aHex;
+	int aHex;
+	stringstream convert(hex);
+	convert >> std::hex >> aHex;
+	return aHex;
+}
+//--------------------------------------------------------------
+static void hexToColor(ofColor &col, string hex) { // must remember to remove # char!
+	string r = hex.substr(0, 2);
+	int ri = stringToHex(r);
+	string g = hex.substr(2, 2);
+	int gi = stringToHex(g);
+	string b = hex.substr(4, 2);
+	int bi = stringToHex(b);
+	col.set(ri, gi, bi);
 }
 
-static void hexToColor(ofColor &col, string hex) {
-    string r = hex.substr(0, 2);
-    int ri = stringToHex(r);
-    string g = hex.substr(2, 2);
-    int gi = stringToHex(g);
-    string b = hex.substr(4, 2);
-    int bi = stringToHex(b);
-    col.set(ri, gi, bi);
-}
-
-class paletteType{
+// tricky types that could be simplified...
+//--------------------------------------------------------------
+class paletteType {
 public:
-    vector<ofColor> palette;
+	vector<ofColor> palette;
 };
 
-class kitType{
+//--------------------------------------------------------------
+class kitType {
 public:
-    vector<paletteType> palettes;
+	vector<paletteType> palettes;
 };
 
-class ofApp : public ofBaseApp{
+//--------------------------------------------------------------
+class ofApp : public ofBaseApp {
 
-	public:
+public:
 
-    kitType kit;
+	kitType kit;
 
-		void setup();
-		void update();
-		void draw();
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
+	void keyPressed(int key);
 
-		ofJson js;
-		ofJson stroke;
+	ofJson js;
 
-		ofTrueTypeFont ttf;
-		std::string text;
-		std::stringstream ss;
+	ofTrueTypeFont ttf;
+	std::string text;
+	std::stringstream ss;
 };
