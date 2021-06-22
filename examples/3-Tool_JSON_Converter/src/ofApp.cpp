3bd719d584108658d96ff550b52d4fb236c7e347
moebius surfing <moebiussurfing@gmail.com>
2021-06-21 21:54:41 -0300

    added google material kit

--- examples/3-Tool_JSON_Converter/src/ofApp.cpp
+++ examples/3-Tool_JSON_Converter/src/ofApp.cpp
@@ -1,7 +1,5 @@
 #include "ofApp.h"
 
-//A JSON of the top color palettes on ColourLovers.com, as RGB hex strings.
-//https://github.com/Jam3/nice-color-palettes
 
 //--------------------------------------------------------------
 void ofApp::setup() {
@@ -9,82 +7,204 @@ void ofApp::setup() {
 
 	ttf.load(OF_TTF_SERIF, 10, true, true, true);
 
-	// set the source file to convert
-	ofFile file("1000.json");
-	//ofFile file("100.json");
+	//--
 
-	string kitName = "FactoryKit";
-	string path = "OUTPUT/" + kitName; // set kit folder name
-	string filenameRoot = "Palette"; // set palette names root
-
-	static bool bDebug = true;
-
-	if (file.exists())
+	// 1.
+	if (0)
 	{
-		file >> js;
+		// Taken from https://github.com/Jam3/nice-color-palettes
+		// A JSON of the top color palettes on ColourLovers.com, as RGB hex strings.
 
-		if (bDebug)
-		{
-			ss.clear();
-			ss << "IMPORTING JSON FILE PALETTES" << endl;
-			ss << "to path: " << path << endl;
-			ss << "with name: " << filenameRoot << "_xx" << endl << endl;
-			ss << js << endl << endl;
-		}
+		// set the source file to convert
+		ofFile file("1000.json");
+		//ofFile file("100.json");
 
-		int ip = 0;
-		for (auto & jsPalette : js)
+		string kitName = "FACTORY KIT";
+		string path = "OUTPUT/" + kitName; // set kit folder name
+		string filenameRoot = "Palette"; // set palette names root
+
+		static bool bDebug = true;
+
+		if (file.exists())
 		{
-			ip++;
-			if (bDebug) {
-				ss << "------------------------" << endl;
-				ss << "palette #" << ip << endl;
+			file >> js;
+
+			if (bDebug)
+			{
+				ss.clear();
+				ss << "IMPORTING JSON FILE PALETTES" << endl;
+				ss << "to path: " << path << endl;
+				ss << "with name: " << filenameRoot << "_xx" << endl << endl;
+				ss << js << endl << endl;
 			}
 
-			if (!jsPalette.empty())
+			int ip = 0;
+			for (auto & jsPalette : js)
 			{
-				paletteType p;
-
-				ofJson jp;
-
-				int ic = 0;
-				for (auto & color : jsPalette)
-				{
-					ofColor myColor;
-					string sColor = color;
-					ofStringReplace(sColor, "#", ""); // remove "#"
-
-					hexToColor(myColor, sColor);
-					if (bDebug) {
-						ss << "color #" << ic++ << " " << sColor << endl;
-					}
-
-					p.palette.push_back(myColor);
-
-					ofJson jc;
-					jc["r"] = myColor.r;
-					jc["g"] = myColor.g;
-					jc["b"] = myColor.b;
-					jc["a"] = myColor.a;
-					jp.push_back(jc);
+				ip++;
+				if (bDebug) {
+					ss << "------------------------" << endl;
+					ss << "palette #" << ip << endl;
 				}
 
-				kit.palettes.push_back(p);
+				if (!jsPalette.empty())
+				{
+					paletteType p;
 
-				string suffix = "";
-				//suffix += ofToString(ip < 1000 ? "0" : "");
-				suffix += ofToString(ip < 100 ? "0" : "");
-				suffix += ofToString(ip < 10 ? "0" : "");
-				suffix += ofToString(ip);
+					ofJson jp;
 
-				string filename = filenameRoot + "_" + suffix + ".json";
+					int ic = 0;
+					for (auto & color : jsPalette)
+					{
+						ofColor myColor;
+						string sColor = color;
+						ofStringReplace(sColor, "#", ""); // remove "#"
 
-				// save json
-				ofSavePrettyJson(path + "/" + filename, jp);
+						hexToColor(myColor, sColor);
+						if (bDebug) {
+							ss << "color #" << ic++ << " " << sColor << endl;
+						}
+
+						p.palette.push_back(myColor);
+
+						ofJson jc;
+						jc["r"] = myColor.r;
+						jc["g"] = myColor.g;
+						jc["b"] = myColor.b;
+						jc["a"] = myColor.a;
+						jp.push_back(jc);
+					}
+
+					kit.palettes.push_back(p);
+
+					string suffix = "";
+					//suffix += ofToString(ip < 1000 ? "0" : "");
+					suffix += ofToString(ip < 100 ? "0" : "");
+					suffix += ofToString(ip < 10 ? "0" : "");
+					suffix += ofToString(ip);
+
+					string filename = filenameRoot + "_" + suffix + ".json";
+
+					// save json
+					ofSavePrettyJson(path + "/" + filename, jp);
+				}
+			}
+
+			ss << "Total processed palettes: " << ip << endl;
+		}
+	}
+
+	//--
+
+	// 2.
+	if (1)
+	{
+		// Taken from
+		// https://github.com/sravanrekandar/google-material-color-palette-json
+
+		ofFile file("palette.json");
+
+		string kitName = "GOOGLE MATERIAL";
+		string path = "OUTPUT/" + kitName; // set kit folder name
+		string filenameRoot = "Palette"; // set palette names root
+
+		static bool bDebug = true;
+
+		if (file.exists())
+		{
+			file >> js;
+
+			if (bDebug)
+			{
+				ss.clear();
+				ss << "IMPORTING JSON FILE PALETTES" << endl;
+				ss << "to path: " << path << endl;
+				ss << "with name: " << filenameRoot << "_xx" << endl << endl;
+				ss << js << endl << endl;
+			}
+
+			//--
+
+			// 2.1 read and build kit data
+
+			int ip = 0;
+			for (ofJson & jsPalette : js)
+			{
+				ip++;
+				if (bDebug) {
+					ss << "----------------------------------------------------------------------" << endl;
+					ss << "palette #" << ip << endl;
+				}
+
+				if (!jsPalette.empty())
+				{
+					paletteType p;
+
+					int ic = 0;
+
+					for (ofJson & color : jsPalette)
+					{
+						if (!color.empty())
+						{
+							//TODO:
+							//this is an nugly workaround that can not get the name..
+
+							string hex = color;
+							ofStringReplace(hex, "#", ""); // remove "#"
+
+							ofColor myColor;
+							hexToColor(myColor, hex);
+							p.palette.push_back(myColor);
+						}
+					}
+
+					kit.palettes.push_back(p);
+				}
+			}
+
+			ss << "Total processed palettes: " << ip << endl;
+
+			//--
+
+			// 2.2 save each palette json
+
+			int ipj = 0;
+
+			for (int p = 0; p < kit.palettes.size(); p++)
+			{
+				if (p != 1 && p != 19) // exclude
+				{
+					ipj++;
+					ofJson jp; // palette
+
+					for (int c = 0; c < kit.palettes[p].palette.size(); c++)
+					{
+						ofColor myColor = kit.palettes[p].palette[c];
+
+						ofJson jc; // color
+
+						jc["r"] = myColor.r;
+						jc["g"] = myColor.g;
+						jc["b"] = myColor.b;
+						jc["a"] = myColor.a;
+						jp.push_back(jc);
+					}
+
+					//--
+
+					string suffix = "";
+					//suffix += ofToString(ipj < 1000 ? "0" : "");
+					//suffix += ofToString(ipj < 100 ? "0" : "");
+					suffix += ofToString(ipj < 10 ? "0" : "");
+					suffix += ofToString(ipj);
+
+					string filename = filenameRoot + "_" + suffix + ".json";
+
+					// save json
+					ofSavePrettyJson(path + "/" + filename, jp);
+				}
 			}
 		}
-
-		ss << "Total converted palettes: " << ip << endl;
 	}
 }
 
